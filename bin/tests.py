c9f62bafea0dc014971fb3c3c23cce6c705528fe
Wilfongjt <wilfongjt@gmail.com>
2024-01-08 12:50:09 -0500

    projectable

--- bin/tests.py
+++ bin/tests.py
@@ -7,6 +7,7 @@ def main():
     able.string_deleter.main()
     able.appendable.main()
     able.classnameable.main()
+    able.projectable.main()
     print('good enough')
 
 if __name__ == "__main__":
