b9350c26b93b3717ae9b39dec99a9c81b24e2e7d
Wilfongjt <wilfongjt@gmail.com>
2024-01-10 16:17:29 -0500

    recordable

--- scripts/05.install.from.gh.sh
+++ scripts/05.install.from.gh.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd ..
 ## Syntax: pip install "Package" @ git+"URL of the repository"
-
+# run this script in the client project
 pip install able@git+https://github.com/Wilfongjt/abilities#egg=lb-lib-0.1.2
