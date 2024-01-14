f1a832e45935fa59f0e209d476b727d99ac8e6a1
Wilfongjt <wilfongjt@gmail.com>
2024-01-14 05:51:26 -0500

    mergeable

--- able/mergeable.py
+++ able/mergeable.py
@@ -4,26 +4,57 @@ class Mergeable():
     ##__Mergable__
     ##
     ## Render a template with user provided values
-    def merge(self, template, nv_list):
+    def __init__(self):
+        self.merged_value = ''
+
+    def create_instance(self):
+        return Mergeable()
+
+    def merge(self, nv_list):
         ##
         ##* Merge name-value pairs into a given template string on request
         # replace found names with values by iteration
+        self.merged_value = ''
+        if isinstance(self, str):
+            self.merged_value = self
         for nv in nv_list:
-            template = template.replace(nv['name'], nv['value'])
+            self.merged_value = self.merged_value.replace(nv['name'], nv['value'])
 
-        return template
+        return self.create_instance()
 
 def main():
-    # setup
-    template= 'Hi from <<A>>, looking at <<B>>.'
-    nv_list=[{'name':'<<A>>', 'value': 'a'},{'name': '<<B>>', 'value': 'b'}]
+    t_str = '''
+    NAME=<<NAME>>
+    DATA=<<DATA>>
+    '''.replace('  ','')
+    expected = '''
+    NAME=name
+    DATA=data
+    '''.replace('  ','')
+    nv_list = [{'name': '<<NAME>>', 'value': 'name'},
+               {'name': '<<DATA>>', 'value': 'data'}]
+    assert(isinstance(Mergeable().merge(nv_list), Mergeable))
+    print(Mergeable().merge(nv_list).merged_value)
+    assert(Mergeable().merge(nv_list).merged_value == '') # nothing to merge into
 
-    # test
-    class Example(Mergeable):
-        def __init__(self):
+    class Example(str, Mergeable):
+        def __init__(self, string_value, nv_list=[]):
             Mergeable.__init__(self)
+        def __new__(cls, string_value, nv_list=[]):
+            merged_value=string_value
+            for nv in nv_list:
+                merged_value = merged_value.replace(nv['name'], nv['value'])
 
-    assert (Example().merge(template, nv_list)=='Hi from a, looking at b.')
+            instance = super().__new__(cls, merged_value)
+            return instance
+
+        def create_instance(self):
+            return Example(self.merged_value)
+
+    assert(Example(t_str)==t_str)
+    assert(Example(t_str, nv_list)==expected)
+    assert(Example(t_str).merge(nv_list)==expected)
+
 
 if __name__ == "__main__":
     # execute as docker
